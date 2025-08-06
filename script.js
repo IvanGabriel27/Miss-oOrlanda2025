7f15cbb8e2524383759983c4ba3d4b261c682c6a
IvanGabriel27 <128513972+IvanGabriel27@users.noreply.github.com>
2025-08-06 09:46:03 -0300

    .

--- script.js
+++ script.js
@@ -4,4 +4,37 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
+const perguntas = [
+    {
+        enunciado:"Quem foi o 1 campeão mundial de surfe?",
+        alternativas:[
+            {
+                texto:"Gabriel Medina",
+                afirmação:"Resposta correta",
+                pontos:1
+            },
+            {
+                texto:"Ítalo Ferreira",
+                afirmação:"Resposta errada",
+                pontos:0
+            }
+        ]
+    },
+       {
+        enunciado:"Quem foi o 1 campeão mundial de surfe?",
+        alternativas:[
+            {
+                texto:"Gabriel Medina",
+                afirmação:"Resposta correta",
+                pontos:1
+            },
+            {
+                texto:"Ítalo Ferreira",
+                afirmação:"Resposta errada",
+                pontos:0
+            }
+        ]
+    },
+]
+
 
